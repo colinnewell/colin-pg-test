19ae4f8f79aed9ccd917f2962d0ccfcd6f47d80f
Colin Newell <colin.newell@gmail.com>
2021-12-04 21:52:49 +0000

    Get postgres largely working

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/colinnewell/pg-init
 
 go 1.17
 
-require github.com/kelseyhightower/envconfig v1.4.0
+require (
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/lib/pq v1.10.4
+)
--- main.go
+++ main.go
@@ -1,28 +1,31 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 
 	"github.com/kelseyhightower/envconfig"
+	_ "github.com/lib/pq"
 )
 
 type Config struct {
-	DSN string `required:true`
+	DSN string `required:"true"`
 }
 
 var pool *sql.DB // Database connection pool.
 
+// MYAPP_DSN="dbname=docker user=docker password=password host=postgres sslmode=disable"
+
 func main() {
 	var c Config
-	fmt.Println("vim-go")
 	err := envconfig.Process("myapp", &c)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	pool, err = sql.Open("driver-name", c.DSN)
+	pool, err = sql.Open("postgres", c.DSN)
 	if err != nil {
 		// This will not be a connection error, but a DSN parse error or
 		// another initialization error.
@@ -30,4 +33,9 @@ func main() {
 	}
 	defer pool.Close()
 
+	if err := pool.PingContext(context.Background()); err != nil {
+		log.Fatalf("unable to connect to database: %v", err)
+	}
+
+	fmt.Println("vim-go")
 }
